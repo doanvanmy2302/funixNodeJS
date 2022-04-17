e62acb99a0029214959d339791b47fef4df3bab8
My <my.dv2002@gmail.com>
2022-04-17 21:52:45 +0700

    lab01_4

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url= req.url;
+    if(url ==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="Message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
