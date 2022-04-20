642bd395439899585091af27e67efc7f5b55b5a5
My <my.dv2002@gmail.com>
2022-04-20 15:14:52 +0700

    lab02_

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const http = require('http');
 const express = require('express');
 const app=express();
 const server = http.createServer(app);
-
+app.use((res,req,next) =>{
+    console.log("this is a middleware");
+    next()
+});
+app.use((res,req,next) =>{
+    console.log("this is a another middleware");
+});
 
 server.listen(3000);
