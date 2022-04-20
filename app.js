468d1630c70dc5e5691413373238550688f5b64a
My <my.dv2002@gmail.com>
2022-04-20 15:57:34 +0700

    lab02_4

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 const app=express();
-
-app.use((req,res,next) =>{
-    console.log("this is a middleware");
+app.use('/',(req,res,next)=>{
+    console.log('this always runs!');
     next();
+});
+app.use('/add-product',(req,res,next) =>{
+    console.log("this is a another middleware");
+    res.send('<h1>the add product</h1>')
 });
 app.use((req,res,next) =>{
     console.log("this is a another middleware");
     res.send('<h1>Hello from express</h1>')
 });
-const server = http.createServer(app);
-server.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000);
+app.listen(3000);
