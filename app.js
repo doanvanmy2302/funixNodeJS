7540e6371f3391c06ea1c267b4c89cd7182b739c
My <my.dv2002@gmail.com>
2022-04-21 08:32:00 +0700

    lab02_5

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 
 // const http = require('http');
 const express = require('express');
+const bodyParser= require('body-parser');
 const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
 app.use('/',(req,res,next)=>{
     console.log('this always runs!');
     next();
 });
 app.use('/add-product',(req,res,next) =>{
-    console.log("this is a another middleware");
-    res.send('<h1>the add product</h1>')
+
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add product</button></form>')
 });
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);res.redirect('/');
+})
+
 app.use((req,res,next) =>{
-    console.log("this is a another middleware");
     res.send('<h1>Hello from express</h1>')
 });
 // const server = http.createServer(app);
