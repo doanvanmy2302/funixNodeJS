7c2766bfda107bbd04720c713681624dc2b3014b
My <my.dv2002@gmail.com>
2022-04-25 20:53:47 +0700

    lab03_4

--- lab03/routes/admin.js
+++ lab03/routes/admin.js
@@ -6,7 +6,7 @@ const rootDir =require('../util/path');
 const products =[];
 router.get('/add-product',(req,res,next) =>{
 
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle: 'add product'});
 });
 router.post('/add-product',(req,res,next)=>{
     products.push({title: req.body.title});
