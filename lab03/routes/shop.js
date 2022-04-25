9bcb313f5f31e0c1b13a07fb5572831a0b4e1a25
My <my.dv2002@gmail.com>
2022-04-25 16:16:04 +0700

    lab03_1

---
+++ lab03/routes/shop.js
@@ -0,0 +1,10 @@
+const path =require('path');
+const express = require('express');
+const router = express.Router();
+const rootDir = require('../util/path')
+const adminData = require('./admin')
+router.get('/',(req,res,next) =>{
+    res.sendFile(path.join(rootDir, 'views','shop.html'));
+    console.log(adminData.products)
+});
+module.exports=router;
