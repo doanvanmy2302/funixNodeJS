9ec413590df97f8a9f6065c658c3fc720a6d13bb
My <my.dv2002@gmail.com>
2022-04-25 20:40:19 +0700

    lab03_3

--- lab03/routes/shop.js
+++ lab03/routes/shop.js
@@ -6,6 +6,7 @@ const adminData = require('./admin')
 router.get('/',(req,res,next) =>{
     // res.sendFile(path.join(rootDir, 'views','shop.html'));
     // console.log(adminData.products)
-    res.render('shop')
+    const  products= adminData.products
+    res.render('shop', {prods: products, docTitle: 'shop'})
 });
 module.exports=router;
