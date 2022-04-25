9831f655df712b9fdfb6e510abe16dd00daa54cc
My <my.dv2002@gmail.com>
2022-04-25 08:50:29 +0700

    lab02_7

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 
-// const http = require('http');
 const express = require('express');
 const bodyParser= require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const app=express();
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/',(req,res,next)=>{
-    console.log('this always runs!');
-    next();
-});
-app.use('/add-product',(req,res,next) =>{
 
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add product</button></form>')
-});
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);res.redirect('/');
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use((req,res,next) =>{
-    res.send('<h1>Hello from express</h1>')
-});
-// const server = http.createServer(app);
-// server.listen(3000);
 app.listen(3000);
