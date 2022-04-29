fe4ad04b9ddc6b8a1c64f623446d488bea1d9a64
My <my.dv2002@gmail.com>
2022-04-29 16:34:37 +0700

    adding the product id to the path

--- lab04/models/product.js
+++ lab04/models/product.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-  path.dirname(process.main.filename),
+  path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
@@ -18,10 +18,15 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
+
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
