76f28846e25b9a708fa5a0ba032d03b00a4d182c
My <my.dv2002@gmail.com>
2022-05-01 11:49:05 +0700

    adding cart models

--- lab04/controllers/shop.js
+++ lab04/controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart = require('../models/cart');
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -39,11 +39,13 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
-exports.postCart =(req, res, next) =>{
-  const proId = req.body.productId;
-  console.log(proId);
-  res.redirect('/cart');
-}
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
