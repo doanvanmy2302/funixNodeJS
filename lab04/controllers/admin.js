8c69566c07f69bd958285d67da92a1b7601e8b15
My <my.dv2002@gmail.com>
2022-05-03 21:17:16 +0700

    lab05-20

--- lab04/controllers/admin.js
+++ lab04/controllers/admin.js
@@ -13,11 +13,13 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const imageUrl = req.body.imageUrl;
     const description = req.body.description;
-    req.user.createProduct({
-        title: title,
-        price: price,
-        imageUrl: imageUrl,
-        description: description,})
+    req.user
+        .createProduct({
+            title: title,
+            price: price,
+            imageUrl: imageUrl,
+            description: description,
+        })
         .then((result) => {
             // console.log(result);
             console.log('Created Product');
@@ -32,8 +34,11 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findByPk(prodId)
-        .then((product) => {
+    req.user
+        .getProducts({ where: { id: prodId } })
+        // Product.findByPk(prodId)
+        .then((products) => {
+            const product = products[0];
             if (!product) {
                 return res.redirect('/');
             }
@@ -69,7 +74,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll()
+    req.user
+        .getProducts()
         .then((products) => {
             res.render('admin/products', {
                 prods: products,
