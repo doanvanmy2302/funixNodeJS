64d743e9fd5dde85142d56335ec9ee4a248af2b3
My <my.dv2002@gmail.com>
2022-05-05 22:27:14 +0700

    creating products

--- lab06/app.js
+++ lab06/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
- 
+ next();
 });
 
 app.use('/admin', adminRoutes);
