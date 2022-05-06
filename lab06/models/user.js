cc229a43359fe59f9b8dc2a652431fbcd4acfedd
My <my.dv2002@gmail.com>
2022-05-06 22:01:23 +0700

    lab06-18

--- lab06/models/user.js
+++ lab06/models/user.js
@@ -14,7 +14,23 @@ save(){
 }
 
 addToCart(product){
-  const updatedCart= {item: [{productId:new mongodb.ObjectId(product._id), quantity: 1}]}
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString();
+  })
+  let newQuantity=1;
+
+  const updatedCartItems=[...this.cart.items];
+  if(cartProductIndex>=0){
+    newQuantity= this.cart.items[cartProductIndex].quantity+1;
+    updatedCartItems[cartProductIndex].quantity= newQuantity;
+  }else{
+    updatedCartItems.push({
+
+      productId: new ObjectId(product._id),
+      quantity: newQuantity,
+    })
+  }
+  const updatedCart= {item: updatedCartItems}
   const db= getDb();
   db.collection('users').updateOne(
     {_id: new mongodb.ObjectId(this._id)}, 
