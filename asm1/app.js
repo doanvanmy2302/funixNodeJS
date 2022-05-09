a7099d0e53cf196f38e1b8f720a6efaa7cedba88
My <my.dv2002@gmail.com>
2022-05-09 07:49:04 +0700

    create app and model MVC

---
+++ asm1/app.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://mydoan:doanvanmy2302@cluster0.ajlp6.mongodb.net/Cluster0?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
