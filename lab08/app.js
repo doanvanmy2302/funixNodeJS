aef741e3eace0afaf0139a0a46631c4b06881f76
My <my.dv2002@gmail.com>
2022-05-18 07:35:43 +0700

    adding the resquest dirven login solution

---
+++ lab08/app.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const authRoutes = require('./routes/auth');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('627750e5545294d3dceb7a8f')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes)
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://mydoan:doanvanmy2302@cluster0.ajlp6.mongodb.net/Cluster0?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+     
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
