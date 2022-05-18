7241a2575beda8bdd14e67971ac1dd57b83f0205
My <my.dv2002@gmail.com>
2022-05-18 08:04:13 +0700

    lab08-4

--- lab08/controllers/auth.js
+++ lab08/controllers/auth.js
@@ -1,15 +1,15 @@
 exports.getLogin= (req, res, next) => {
-    const isLoggedIn=req.get('Cookie')
-    .split(';')[1]
-    .trim()
-    .split('=')[1] === true;
+    // const isLoggedIn=req.get('Cookie')
+    // .split(';')[1]
+    // .trim()
+    // .split('=')[1] ;
     res.render('auth/login',{
      path:'/login',
      pageTitle:'Login',
-     isAuthenticated: isLoggedIn
+     isAuthenticated: false
     })
 }
 exports.postLogin= (req,res,next)=>{
-  res.setHeader('Set-Cookie','isLoggedIn=true')
+  res.setHeader('Set-Cookie','isLoggedIn=true; httpOnly')
     res.redirect('/')
 }
