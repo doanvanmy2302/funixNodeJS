b4397eb44ce479df98f1ff9fe90ba2cf32560b5d
My <my.dv2002@gmail.com>
2022-05-18 09:14:58 +0700

    lab08-8

--- lab08/controllers/auth.js
+++ lab08/controllers/auth.js
@@ -1,3 +1,5 @@
+const User= require('../models/user')
+
 exports.getLogin = (req, res, next) => {
   //   const isLoggedIn = req
   //     .get('Cookie')
@@ -13,6 +15,17 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+    User.findById('627750e5545294d3dceb7a8f')
+    .then((user) => {
+       req.session.isLoggedIn = true; 
+        req.session.user=user;
+         res.redirect('/');
+    })
+  .catch((err)=>{console.log(err)})
+ 
 };
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err)=>{console.log(err)
+    res.redirect('/');
+    })
+}
