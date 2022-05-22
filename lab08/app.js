fd10519d4851f75965b9909256dabdb00d92d77c
My <my.dv2002@gmail.com>
2022-05-22 11:30:43 +0700

    lab08-24

--- lab08/app.js
+++ lab08/app.js
@@ -40,13 +40,6 @@ app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
-app.use((req,res,next)=>{
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.session.csrfToken();
-  next()
-  
-})
-
   User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
@@ -54,7 +47,12 @@ app.use((req,res,next)=>{
     })
     .catch(err => console.log(err));
 });
-
+app.use((req,res,next)=>{
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next()
+  
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
