65d38ed54cc46ac9ab69cbbf1d16b6dc63094b75
My <my.dv2002@gmail.com>
2022-05-22 18:28:08 +0700

    lab09-7

--- lab08/controllers/auth.js
+++ lab08/controllers/auth.js
@@ -40,7 +40,12 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+  },
   });
 };
 
@@ -78,26 +83,21 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput:{
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword
+      }
     });
   }
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash(
-          'error',
-          'E-Mail exists already, please pick a different one.'
-        );
-        return res.redirect('/signup');
-      }
-      return bcrypt
+   bcrypt
         .hash(password, 12)
         .then(hashedPassword => {
           const user = new User({
@@ -119,10 +119,7 @@ exports.postSignup = (req, res, next) => {
         .catch(err => {
           console.log(err);
         });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    
 };
 
 exports.postLogout = (req, res, next) => {
--- lab08/routes/auth.js
+++ lab08/routes/auth.js
@@ -16,10 +16,12 @@ router.post('/signup',  [
         .isEmail()
         .withMessage('Please enter a valid email!')
         .custom((value, { req }) => {
-            if (value === 'test@test.com') {
-                throw new Error('This email address is forbidden');
-            }
-            return true;
+            return User.findOne({email:value})
+            .then((userDoc)=>{
+                if(userDoc){
+                    return Promise.reject('Email exists already, please pick a different one')
+                }
+            })
         }),
     body(
         'password',
