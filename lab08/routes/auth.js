53a707de5021d6083800c01e0bf4f0d39568b3e4
My <my.dv2002@gmail.com>
2022-05-22 16:49:08 +0700

    lab09-4

--- lab08/routes/auth.js
+++ lab08/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -11,14 +11,23 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail()
-.withMessage('Please enter valid email')
-.custom((value,{req}) =>{
-    if(value==='test@test.com'){
-        throw new Error('this email address if forbidden')
-    }
-    return true
-})
+router.post('/signup',  [
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email!')
+        .custom((value, { req }) => {
+            if (value === 'test@test.com') {
+                throw new Error('This email address is forbidden');
+            }
+            return true;
+        }),
+    body(
+        'password',
+        'Please enter a password with only numbers and text and at least 5 characters'
+    )
+        .isLength({ min: 5 })
+        .isAlphanumeric(),
+]
 , authController.postSignup);
 
 router.post('/logout', authController.postLogout);
