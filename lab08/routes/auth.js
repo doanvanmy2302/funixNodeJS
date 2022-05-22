f26ad2de7040d5b5d9181fd2806cf124f089911e
My <my.dv2002@gmail.com>
2022-05-22 15:38:20 +0700

    lab09-3

--- lab08/routes/auth.js
+++ lab08/routes/auth.js
@@ -11,7 +11,15 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter valid email'), authController.postSignup);
+router.post('/signup', check('email').isEmail()
+.withMessage('Please enter valid email')
+.custom((value,{req}) =>{
+    if(value==='test@test.com'){
+        throw new Error('this email address if forbidden')
+    }
+    return true
+})
+, authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
