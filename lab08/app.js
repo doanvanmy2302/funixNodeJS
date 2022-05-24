aef65d0b3ac5afde5f6cb4d1ac363f4d1d3358dc
My <my.dv2002@gmail.com>
2022-05-24 18:03:30 +0700

    lab10-8

--- lab08/app.js
+++ lab08/app.js
@@ -48,6 +48,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 app.use(session({secret: 'my secret',resave:false,
 saveUninitialized: false, store:store
