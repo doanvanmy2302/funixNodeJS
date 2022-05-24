e129dbf88fe2f74b1dbcea5c10f957d2e8b01d81
My <my.dv2002@gmail.com>
2022-05-24 12:04:25 +0700

    lab10-1

--- lab08/routes/auth.js
+++ lab08/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -9,53 +10,68 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', [
-  body('email')
-  .isEmail()
-  .normalizeEmail()
-  .withMessage('please enter valid email address'),
-  body('password')
-  .isLength({min: 5})
-  .isAlphanumeric().trim()
-], authController.postLogin);
+router.post(
+  '/login',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email address.')
+      .normalizeEmail(),
+    body('password', 'Password has to be valid.')
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim()
+  ],
+  authController.postLogin
+);
 
-router.post('/signup',  [
+router.post(
+  '/signup',
+  [
     check('email')
-        .isEmail().normalizeEmail()
-        .withMessage('Please enter a valid email!')
-        .custom((value, { req }) => {
-            return User.findOne({email:value})
-            .then((userDoc)=>{
-                if(userDoc){
-                    return Promise.reject('Email exists already, please pick a different one')
-                }
-            })
-        }),
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address if forbidden.');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-Mail exists already, please pick a different one.'
+            );
+          }
+        });
+      })
+      .normalizeEmail(),
     body(
-        'password',
-        'Please enter a password with only numbers and text and at least 5 characters'
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters.'
     )
-        .isLength({ min: 5 }).trim()
-        .isAlphanumeric(),
-        body('confirmPassword')
-        .trim()
-        .custom((value, { req }) => {
-            if (value !== req.body.password) {
-                throw new Error('Passwords have to match');
-            }
-            return true;
-        }),
-]
-, authController.postSignup);
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
+  ],
+  authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
-// router.get('/reset', authController.getReset);
+router.get('/reset', authController.getReset);
 
-// router.post('/reset', authController.postReset);
+router.post('/reset', authController.postReset);
 
-// router.get('/reset/:token', authController.getNewPassword);
+router.get('/reset/:token', authController.getNewPassword);
 
-// router.post('/new-password', authController.postNewPassword);
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
