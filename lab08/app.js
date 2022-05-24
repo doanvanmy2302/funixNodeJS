1d40d08fe925e603699e0842ccedc18fe46b1037
My <my.dv2002@gmail.com>
2022-05-24 14:02:56 +0700

    lab10-5

--- lab08/app.js
+++ lab08/app.js
@@ -28,7 +28,17 @@ const fileStorage =multer.diskStorage({
     cb(null, new Date().toISOString()+'-'+ file.originalname)
   },
 })
-
+const fileFilter = (req, file, cb) => {
+  if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+  ) {
+      cb(null, true);
+  } else {
+      cb(null, false);
+  }
+};
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -38,7 +48,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(multer({storage: fileStorage}).single('image'))
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 app.use(session({secret: 'my secret',resave:false,
 saveUninitialized: false, store:store
 }))
