b80ea055d8561ffc30c88bd43bf9444be44fb2ec
My <my.dv2002@gmail.com>
2022-05-24 19:02:36 +0700

    lab10-14

--- lab08/controllers/shop.js
+++ lab08/controllers/shop.js
@@ -171,7 +171,26 @@ exports.getInvoice = (req, res, next) => {
             pdfDoc.pipe(fs.createWriteStream(invoicePath));
             pdfDoc.pipe(res);
 
-            pdfDoc.text('Hello World!');
+            pdfDoc.fontSize(26).text('Invoice', {
+                underline: true,
+            });
+            pdfDoc.text('---------------------');
+            let totalPrice = 0;
+            order.products.forEach((prod) => {
+                totalPrice += prod.quantity * prod.product.price;
+                pdfDoc
+                    .fontSize(14)
+                    .text(
+                        prod.product.title +
+                            ' - ' +
+                            prod.quantity +
+                            ' x ' +
+                            '$' +
+                            prod.product.price
+                    );
+            });
+            pdfDoc.text('---------------------');
+            pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
 
             pdfDoc.end();
             // fs.readFile(invoicePath, (err, data) => {
@@ -191,4 +210,4 @@ exports.getInvoice = (req, res, next) => {
             // file.pipe(res);
         })
         .catch((err) => next(err));
-};
+ }
